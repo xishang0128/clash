797e3d18d8626a2aa1c3516ee4582aa4b4d945ad
xishang0128 <xishang02@gmail.com>
2023-04-19 00:37:10 +0800

    1

--- installcore.sh
+++ installcore.sh
@@ -10,19 +10,19 @@ version=`echo $url | awk -F '.' '{print $4}' | awk -F '-' '{print $NF}'`
 size=`curl -s  https://api.github.com/repos/MetaCubeX/Clash.Meta/releases/tags/Prerelease-Alpha | grep -B 5 "download/Prerelease-Alpha/clash.meta-$os-$arch-alpha" | grep size | awk -F ':' '{print $2}' | awk -F ',' '{print $1}'`
 
 update() {
-wget -O ./clash.gz $url
+wget -O ./clashcore.gz $url
 
-filesize=`stat ./clash.gz | grep Size | awk -F ' ' '{print $2}'`
+filesize=`stat ./clashcore.gz | grep Size | awk -F ' ' '{print $2}'`
 if [ -z $filesize ]; then
-  filesize=`stat ./clash.gz | grep 大小 | awk -F ' ' '{print $1}' | awk -F '：' '{print $2}'`
+  filesize=`stat ./clashcore.gz | grep 大小 | awk -F ' ' '{print $1}' | awk -F '：' '{print $2}'`
 elif [ -z $filesize ]; then
-  filesize=`stat ./clash.gz | awk -F '"' '{print $1}' | awk -F ' ' '{print $NF}'`
+  filesize=`stat ./clashcore.gz | awk -F '"' '{print $1}' | awk -F ' ' '{print $NF}'`
 fi
 
 if [ $size = $filesize ]; then
-  gzip -d ./clash.gz
+  gzip -d ./clashcore.gz
   ls -al
-  mv ./clash ./clash/bin/clash
+  mv ./clashcore ./clash/bin/clash
 else
   echo 更新失败了喵,压缩包校验失败
   exit 1
